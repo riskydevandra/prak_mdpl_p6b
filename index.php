836b20b4f6c2c36cd997318e39ad78ed45e71c72
riskydevandra1453.gmail.com <riskydevandra1453@gmail.com>
2021-11-02 12:18:19 +0700

    menambah file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MDPL Prak_P6</title>
+</head>
+<body>
+    <h3>materi git remote</h3>
+</body>
+</html>
